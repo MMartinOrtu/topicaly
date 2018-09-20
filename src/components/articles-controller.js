35bf844ba247a84484ed2c0727c7ff59c2029e03
Miguel Martin <pomikelor@gmail.com>
2018-09-20 14:24:00 +0200

    Format lis of articles

--- src/components/articles-controller.js
+++ src/components/articles-controller.js
@@ -1,5 +1,7 @@
 import { ArticlesService } from 'services/articles-service.js';
 
+import placeholder from 'assets/user-solid.svg';
+
 export class ArticlesController {
     constructor (selector, articlesService){
         this.element= document.querySelector(selector);
@@ -12,15 +14,19 @@ export class ArticlesController {
         this.articlesService.getArticles().then( articles =>{
             let html = '';
             for (let article of articles) {
-            html += `<article class="article">
-                <div class="cover">
-                    <img src="${article.cover}" alt="${article.artist} - ${article.title}">
-                </div>
-                <div class="info">
-                    <div class="title">${article.title}</div>
-                    <div class="artist">${article.artist}</div>
-                </div>
-            </article>`;
+            html += `<div class="articles">
+                        <div class="cover">
+                            <img src="${article.cover}">
+                        </div>
+                        <div class="info">
+                            <h2 class="title">${article.title}</h2>
+                            <div class="text">text</div>
+                            <div class="author">${article.author}</div>
+                            <img  class="profile-picture" src="${placeholder}" srcset="${article.picture}" width="100px">
+                            <time class="published-date">${article.added}</time>
+                            <a href="">Comentarios</a>
+                        </div>
+                </div>`;
         }
         this.element.innerHTML = html;
     });
